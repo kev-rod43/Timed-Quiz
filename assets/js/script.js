57ff5164e9d21cabbdb41984110b3b8d9fde3b3e
Kev-rod43 <kev.rod43@yahoo.com>
2023-11-20 16:29:26 -0500

    completed all buttons and highscore board behaviours, ass well as all localstorage management

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
+let goToHighScores = document.querySelector("#go-to-high-scores");
+goToHighScores.style.cursor = "pointer";
 let main = document.querySelector("main");
+let footer = document.querySelector("footer");
 let initial = document.querySelector("#initial");
-let final = document.querySelector("#final")
+let final = document.querySelector("#final");
+let currentScore = document.querySelector("#current-score");
 let highScoresCard = document.querySelector("#high-scores-card");
 let submitScore = document.querySelector("#submit-score");
 let questionCard = document.getElementsByClassName("question-card");
@@ -9,9 +13,33 @@ let time = document.querySelector("#timer-value");
 let wrongAnswers = document.getElementsByClassName("wrong-answer");
 let rightAnswers = document.getElementsByClassName("right-answer");
 
+let savedHighScores= new Object;
+if (JSON.parse(localStorage.getItem("saved-high-scores"))!==null){
+    savedHighScores= JSON.parse(localStorage.getItem("saved-high-scores"));
+}
+
+document.querySelector("#home").addEventListener("click", function(){location.reload()})
+
+document.querySelector("#erase-high-scores").addEventListener("click", function(){localStorage.removeItem("saved-high-scores"); location.reload()})
+
+goToHighScores.addEventListener("click", createHighScoreLines)
+
 main.replaceChildren(initial)
 
 startButton.addEventListener("click", timedQuiz)
+
+function createHighScoreLines(){
+    let h2Element = document.createElement("h2")
+    let sortedHighScores = Object.entries(savedHighScores).sort((a, b) => b[1] - a[1]);
+    localStorage.setItem("saved-high-scores",JSON.stringify(savedHighScores));
+    for (i=0; i<sortedHighScores.length; i++){
+        tempElement = h2Element.cloneNode(h2Element)
+        tempElement.textContent = sortedHighScores[i][0] + ": " + sortedHighScores[i][1];
+        document.querySelector("#high-scores-list").appendChild(tempElement);
+        document.querySelector("header").textContent="";
+    }
+    main.replaceChildren(highScoresCard);
+}
 
 function timedQuiz(){
 
@@ -19,18 +47,63 @@ function timedQuiz(){
         seconds -= 10;
         if (questionCard.length>1){
             main.replaceChildren(questionCard[1])
+            footer.textContent = "Wrong!";
         } else {
-            main.replaceChildren(final)
+            currentScore.textContent = "Score:" + score;
+            seconds = 0;
+            main.replaceChildren(final);
+            footer.textContent = "Wrong!";
+            setTimeout ( function(){footer.textContent = "";}, 2000)
         }
     }
 
-function rightResponse(){
+    function rightResponse(){
+    score += 1;
     if (questionCard.length>1){
         main.replaceChildren(questionCard[1])
+        footer.textContent = "Correct!";
     } else {
-        main.replaceChildren(final)
+        currentScore.textContent = "Score:" + score;
+        seconds = 0;
+        main.replaceChildren(final);
+        footer.textContent = "Correct!";
+        setTimeout ( function(){footer.textContent = "";}, 2000)
     }
-}
+    }
+//validates the highscore submission, and populates the highscores in the highscore card
+submitScore.addEventListener("click", function(event){
+    let initials = document.querySelector("#initials").value.toUpperCase();
+    event.preventDefault();
+
+    
+    
+    if (isNaN(initials) && initials.length==2){
+    } else {
+        alert("Your initials must be 2 alpabetical Characters");
+        return;
+    }
+    if (savedHighScores === null){
+        
+        //delete savedHighScores.null
+        //let savedHighScores= new Object;
+        savedHighScores[initials]= score;
+        localStorage.setItem("saved-high-scores",JSON.stringify(savedHighScores));
+        createHighScoreLines()
+
+    } else if (!(initials in savedHighScores)){
+
+        savedHighScores[initials] = score;
+        createHighScoreLines()
+
+    } else if (confirm("These initials already exist, would you like to overwrite your previous score?")){
+
+        savedHighScores[initials] = score;
+        createHighScoreLines()
+
+    } else {
+        createHighScoreLines()
+    }
+})
 
 for (i=0; i<wrongAnswers.length;i++){
     wrongAnswers[i].addEventListener("click",wrongResponse)
@@ -43,6 +116,8 @@ for (i=0; i<rightAnswers.length;i++){
 }
 
     main.replaceChildren(questionCard[0])
+
+    let score = 0;
 
     let seconds = 70;
 
@@ -59,4 +134,3 @@ for (i=0; i<rightAnswers.length;i++){
     }, 1000)
 
 }
-console.log(main)
